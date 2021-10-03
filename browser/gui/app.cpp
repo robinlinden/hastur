robinlinden/hastur

commit b39f178426dbcbe40ff744c8628db25b2667068a

browser/gui: Use the new painter API for scrolling

browser/gui/app.cpp
@@ -16,16 +16,6 @@
 #include <imgui_stdlib.h>
 #include <spdlog/spdlog.h>
 
-// MSVC gl.h doesn't include everything it uses.
-#ifdef _MSC_VER
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif // WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#endif // _MSC_VER
-
-#include <GL/gl.h>
-
 #include <iterator>
 #include <string_view>
 #include <utility>
@@ -257,13 +247,14 @@ layout::Position App::to_document_position(layout::Position window_position) con
 }
 
 void App::reset_scroll() {
+    painter_.add_translation(0, -scroll_offset_y_);
     scroll_offset_y_ = 0;
     render::render_setup(window_.getSize().x, window_.getSize().y);
 }
 
 void App::scroll(int pixels) {
-    glTranslatef(0, static_cast<float>(pixels), 0);
-    scroll_offset_y_ -= pixels;
+    painter_.add_translation(0, pixels);
+    scroll_offset_y_ += pixels;
 }
 
 void App::run_overlay() {

commit b56b010db71f2ca92d536c2a8a9a31d80eacce41

browser/gui: Fix mouse document position being incorrect after scrolling
This issue was introduced in b39f178426dbcbe40ff744c8628db25b2667068a
where the stored scroll offset was inverted to go with the new IPainter
translation API, but this wasn't taken into account in the mouse
translation calculation.

browser/gui/app.cpp
@@ -243,7 +243,7 @@ std::string App::get_hovered_element_text(layout::Position p) const {
 }
 
 layout::Position App::to_document_position(layout::Position window_position) const {
-    return {window_position.x, window_position.y + scroll_offset_y_};
+    return {window_position.x, window_position.y - scroll_offset_y_};
 }
 
 void App::reset_scroll() {