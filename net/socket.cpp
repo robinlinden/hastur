74c4d684daa92fcc59b942b104b0011cbe628792
Mikael Larsson <mikael.larsson@epiroc.com>
2021-11-03 22:21:57 +0100

    net: Add a base class for socket impl

--- net/socket.cpp
+++ net/socket.cpp
@@ -10,36 +10,38 @@
 namespace net {
 namespace {
 
-bool connect_impl(asio::ip::tcp::resolver &resolver,
-        asio::ip::tcp::socket &socket,
-        std::string_view host,
-        std::string_view service) {
-    asio::error_code ec;
-    auto endpoints = resolver.resolve(host, service, ec);
-    asio::connect(socket, endpoints, ec);
-    return !ec;
-}
+struct BaseSocketImpl {
+    bool connect(asio::ip::tcp::resolver &resolver,
+            asio::ip::tcp::socket &socket,
+            std::string_view host,
+            std::string_view service) {
+        asio::error_code ec;
+        auto endpoints = resolver.resolve(host, service, ec);
+        asio::connect(socket, endpoints, ec);
+        return !ec;
+    }
 
-std::size_t write_impl(auto &socket, std::string_view data) {
-    asio::error_code ec;
-    return asio::write(socket, asio::buffer(data), ec);
-}
+    std::size_t write(auto &socket, std::string_view data) {
+        asio::error_code ec;
+        return asio::write(socket, asio::buffer(data), ec);
+    }
 
-std::string read_all_impl(auto &socket) {
-    std::string data;
-    asio::error_code ec;
-    asio::read(socket, asio::dynamic_buffer(data), ec);
-    return data;
-}
+    std::string read_all(auto &socket) {
+        std::string data;
+        asio::error_code ec;
+        asio::read(socket, asio::dynamic_buffer(data), ec);
+        return data;
+    }
 
-std::size_t read_until_impl(auto &socket, std::string &data, std::string_view delimiter) {
-    asio::error_code ec;
-    return asio::read_until(socket, asio::dynamic_buffer(data), delimiter, ec);
-}
+    std::size_t read_until(auto &socket, std::string &data, std::string_view delimiter) {
+        asio::error_code ec;
+        return asio::read_until(socket, asio::dynamic_buffer(data), delimiter, ec);
+    }
+};
 
 } // namespace
 
-struct Socket::Impl {
+struct Socket::Impl : public BaseSocketImpl {
     Impl() : resolver(svc), socket(svc) {}
 
     asio::io_service svc{};
@@ -56,26 +58,26 @@ Socket::Socket(Socket &&) = default;
 Socket &Socket::operator=(Socket &&) = default;
 
 bool Socket::connect(std::string_view host, std::string_view service) {
-    return connect_impl(impl_->resolver, impl_->socket, host, service);
+    return impl_->connect(impl_->resolver, impl_->socket, host, service);
 }
 
 std::size_t Socket::write(std::string_view data) {
-    return write_impl(impl_->socket, data);
+    return impl_->write(impl_->socket, data);
 }
 
 std::string Socket::read_all() {
-    return read_all_impl(impl_->socket);
+    return impl_->read_all(impl_->socket);
 }
 
 std::size_t Socket::read_until(std::string &data, std::string_view delimiter) {
-    return read_until_impl(impl_->socket, data, delimiter);
+    return impl_->read_until(impl_->socket, data, delimiter);
 }
 
-struct SecureSocket::Impl {
+struct SecureSocket::Impl : public BaseSocketImpl {
     Impl() : resolver(svc), ctx(asio::ssl::context::method::sslv23_client), socket(svc, ctx) {}
 
     bool connect(std::string_view host, std::string_view service) {
-        if (connect_impl(resolver, socket.next_layer(), host, service)) {
+        if (BaseSocketImpl::connect(resolver, socket.next_layer(), host, service)) {
             socket.handshake(asio::ssl::stream_base::handshake_type::client);
             return true;
         }
@@ -101,15 +103,15 @@ bool SecureSocket::connect(std::string_view host, std::string_view service) {
 }
 
 std::size_t SecureSocket::write(std::string_view data) {
-    return write_impl(impl_->socket, data);
+    return impl_->write(impl_->socket, data);
 }
 
 std::string SecureSocket::read_all() {
-    return read_all_impl(impl_->socket);
+    return impl_->read_all(impl_->socket);
 }
 
 std::size_t SecureSocket::read_until(std::string &data, std::string_view delimiter) {
-    return read_until_impl(impl_->socket, data, delimiter);
+    return impl_->read_until(impl_->socket, data, delimiter);
 }
 
 } // namespace net
