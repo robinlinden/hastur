64f9d398da2fccf9e64b7a069c0622ce302e71f9
Mikael Larsson <mikael.larsson@epiroc.com>
2021-11-07 18:45:12 +0100

    util: Add function to trim whitespaces from start/end of string

--- util/string_test.cpp
+++ util/string_test.cpp
@@ -9,7 +9,11 @@
 using namespace std::literals;
 
 using etest::expect;
+using etest::expect_eq;
 using util::no_case_compare;
+using util::trim;
+using util::trim_end;
+using util::trim_start;
 
 int main() {
     etest::test("no case compare", [] {
@@ -23,6 +27,31 @@ int main() {
         expect(!no_case_compare(" word"sv, "word"sv));
         expect(!no_case_compare("word "sv, "word"sv));
         expect(!no_case_compare("word "sv, "woord"sv));
+    });
+
+    etest::test("trim start", [] {
+        expect_eq(trim_start(" abc "), "abc ");
+        expect_eq(trim_start("\t431\r\n"), "431\r\n");
+        expect_eq(trim_start("  hello world!"), "hello world!");
+        expect_eq(trim_start("word "), "word ");
+        expect_eq(trim_start("\r\n"), "");
+    });
+
+    etest::test("trim end", [] {
+        expect_eq(trim_end("abc "), "abc");
+        expect_eq(trim_end("53 \r\n"), "53");
+        expect_eq(trim_end("hello world!\t"), "hello world!");
+        expect_eq(trim_end(" word"), " word");
+        expect_eq(trim_end("\r\n"), "");
+    });
+
+    etest::test("trim", [] {
+        expect_eq(trim("abc"), "abc");
+        expect_eq(trim("\t431"), "431");
+        expect_eq(trim("53 \r\n"), "53");
+        expect_eq(trim("\t\thello world\n"), "hello world");
+        expect_eq(trim(" a b c d "), "a b c d");
+        expect_eq(trim("\r\n"), "");
     });
 
     return etest::run_all_tests();
