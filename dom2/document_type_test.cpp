4a943469dfc5417068d4b25049c8ebcf52dc5abe
Robin Linden <dev@robinlinden.eu>
2021-11-27 16:41:16 +0100

    dom2: Finish DocumentType implementation

--- dom2/document_type_test.cpp
+++ dom2/document_type_test.cpp
@@ -6,14 +6,36 @@
 
 #include "etest/etest.h"
 
+#include <string>
+#include <string_view>
+
+using namespace std::literals;
+using etest::expect;
 using etest::expect_eq;
 
 using namespace dom2;
 
 int main() {
     etest::test("type", [] {
-        DocumentType node{};
+        DocumentType node{"html"s};
         expect_eq(node.type(), NodeType::DocumentType);
+    });
+
+    etest::test("construction", [] {
+        DocumentType node{"html"s};
+        expect_eq(node.name(), "html"sv);
+        expect(node.public_id().empty());
+        expect(node.system_id().empty());
+
+        DocumentType node_with_public_id{"good name"s, "some id"s};
+        expect_eq(node_with_public_id.name(), "good name"sv);
+        expect_eq(node_with_public_id.public_id(), "some id"sv);
+        expect(node_with_public_id.system_id().empty());
+
+        DocumentType node_with_public_and_system_id{"good name"s, "some id"s, "another id"};
+        expect_eq(node_with_public_and_system_id.name(), "good name"sv);
+        expect_eq(node_with_public_and_system_id.public_id(), "some id"sv);
+        expect_eq(node_with_public_and_system_id.system_id(), "another id"sv);
     });
 
     return etest::run_all_tests();
