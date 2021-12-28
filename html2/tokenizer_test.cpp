a919ec307449ef478a07b531760927e34e7432da
Mikael Larsson <mikael.larsson@epiroc.com>
2021-12-28 14:40:14 +0100

    html2: Fix so that eof token is emitted from tokenizer
    This commit fixes so that the loop in the tokenizer does
    not break prematurley. Prior to this change there were no
    eof tokens emitted by the tokenizer. This was due to that
    the state machine never got the chance to act on an EOF
    event.

--- html2/tokenizer_test.cpp
+++ html2/tokenizer_test.cpp
@@ -44,31 +44,32 @@ int main() {
                         StartTagToken{.tag_name = "html"s},
                         CharacterToken{'\n'},
                         EndTagToken{.tag_name = "html"s},
-                        CharacterToken{'\n'}});
+                        CharacterToken{'\n'},
+                        EndOfFileToken{}});
     });
 
     etest::test("comment, simple", [] {
         auto tokens = run_tokenizer("<!-- Hello -->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = " Hello "}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = " Hello "}, EndOfFileToken{}});
     });
 
     etest::test("comment, empty", [] {
         auto tokens = run_tokenizer("<!---->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = ""}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = ""}, EndOfFileToken{}});
     });
 
     etest::test("comment, with dashes and bang", [] {
         auto tokens = run_tokenizer("<!--!-->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "!"}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "!"}, EndOfFileToken{}});
     });
 
     etest::test("comment, with new lines", [] {
         auto tokens = run_tokenizer("<!--\nOne\nTwo\n-->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "\nOne\nTwo\n"}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "\nOne\nTwo\n"}, EndOfFileToken{}});
     });
 
     etest::test("comment, multiple with new lines", [] {
@@ -79,19 +80,21 @@ int main() {
                         CharacterToken{'\n'},
                         CommentToken{.data = "b"},
                         CharacterToken{'\n'},
-                        CommentToken{.data = "c"}});
+                        CommentToken{.data = "c"},
+                        EndOfFileToken{}});
     });
 
     etest::test("comment, allowed to end with <!", [] {
         auto tokens = run_tokenizer("<!--My favorite operators are > and <!-->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "My favorite operators are > and <!"}});
+        expect_eq(tokens,
+                std::vector<Token>{CommentToken{.data = "My favorite operators are > and <!"}, EndOfFileToken{}});
     });
 
     etest::test("comment, nested comment", [] {
         auto tokens = run_tokenizer("<!--<!---->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "<!--"}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "<!--"}, EndOfFileToken{}});
     });
 
     etest::test("comment, nested comment closed", [] {
@@ -102,40 +105,39 @@ int main() {
                         CharacterToken{' '},
                         CharacterToken{'-'},
                         CharacterToken{'-'},
-                        CharacterToken{'>'}});
+                        CharacterToken{'>'},
+                        EndOfFileToken{}});
     });
 
     etest::test("comment, abrupt closing in comment start", [] {
         auto tokens = run_tokenizer("<!-->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = ""}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = ""}, EndOfFileToken{}});
     });
 
     etest::test("comment, abrupt closing in comment start dash", [] {
         auto tokens = run_tokenizer("<!--->");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = ""}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = ""}, EndOfFileToken{}});
     });
 
     etest::test("comment, incorrectly closed comment", [] {
         auto tokens = run_tokenizer("<!--abc--!>");
 
-        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "abc"}});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "abc"}, EndOfFileToken{}});
     });
 
-    // TODO(mkiael): Enable when eof check is fixed
-    // etest::test("comment, end before comment", [] {
-    //    auto tokens = run_tokenizer("<!--");
-    //
-    //    expect_eq(tokens, std::vector<Token>{EndOfFileToken{}});
-    //});
+    etest::test("comment, end before comment", [] {
+        auto tokens = run_tokenizer("<!--");
 
-    // TODO(mkiael): Enable when eof check is fixed
-    // etest::test("comment, eof before comment is closed", [] {
-    //    auto tokens = run_tokenizer("<!--abc");
-    //
-    //    expect_eq(tokens, std::vector<Token>{CommentToken{.data = "abc"}, EndOfFileToken{}});
-    //});
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = ""}, EndOfFileToken{}});
+    });
+
+    etest::test("comment, eof before comment is closed", [] {
+        auto tokens = run_tokenizer("<!--abc");
+
+        expect_eq(tokens, std::vector<Token>{CommentToken{.data = "abc"}, EndOfFileToken{}});
+    });
 
     return etest::run_all_tests();
 }
