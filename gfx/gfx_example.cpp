25c528e1c9cf22ff56cbaf5a686d7cfa43a94313
Robin Linden <dev@robinlinden.eu>
2022-02-22 21:56:04 +0100

    gfx: Support running the example using either gfx backend

--- gfx/gfx_example.cpp
+++ gfx/gfx_example.cpp
@@ -1,20 +1,32 @@
-// SPDX-FileCopyrightText: 2021 Robin Lindén <dev@robinlinden.eu>
+// SPDX-FileCopyrightText: 2021-2022 Robin Lindén <dev@robinlinden.eu>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
 #include "gfx/color.h"
 #include "gfx/opengl_painter.h"
+#include "gfx/sfml_painter.h"
 
+#include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
-#include <SFML/Window/Window.hpp>
 
-int main() {
-    sf::Window window{sf::VideoMode{800, 600}, "gfx"};
+#include <memory>
+#include <string_view>
+
+using namespace std::literals;
+
+int main(int argc, char **argv) {
+    sf::RenderWindow window{sf::VideoMode{800, 600}, "gfx"};
     window.setVerticalSyncEnabled(true);
     window.setActive(true);
 
-    gfx::OpenGLPainter painter{};
-    painter.set_viewport_size(window.getSize().x, window.getSize().y);
+    auto painter = [&]() -> std::unique_ptr<gfx::IPainter> {
+        if (argc == 2 && argv[1] == "--sf"sv) {
+            return std::make_unique<gfx::SfmlPainter>(window);
+        }
+        return std::make_unique<gfx::OpenGLPainter>();
+    }();
+
+    painter->set_viewport_size(window.getSize().x, window.getSize().y);
 
     bool running = true;
     while (running) {
@@ -25,7 +37,7 @@ int main() {
                     running = false;
                     break;
                 case sf::Event::Resized:
-                    painter.set_viewport_size(event.size.width, event.size.height);
+                    painter->set_viewport_size(event.size.width, event.size.height);
                     break;
                 default:
                     break;
@@ -36,10 +48,10 @@ int main() {
         auto x = static_cast<int>(size.x);
         auto y = static_cast<int>(size.y);
 
-        painter.fill_rect({0, 0, x, y}, gfx::Color{0xFF, 0xFF, 0xFF});
+        painter->fill_rect({0, 0, x, y}, gfx::Color{0xFF, 0xFF, 0xFF});
 
-        painter.fill_rect({200, 200, 100, 100}, gfx::Color{0, 0, 0xAA});
-        painter.fill_rect({x / 4 + 50, y / 3 + 50, x / 2, y / 3}, gfx::Color{0xAA, 0, 0, 0x33});
+        painter->fill_rect({200, 200, 100, 100}, gfx::Color{0, 0, 0xAA});
+        painter->fill_rect({x / 4 + 50, y / 3 + 50, x / 2, y / 3}, gfx::Color{0xAA, 0, 0, 0x33});
 
         window.display();
     }
