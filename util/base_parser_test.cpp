bc1826808fd6e1367fc6bc7dac496649c610fc85
Robin Linden <dev@robinlinden.eu>
2022-06-09 22:45:58 +0200

    util/base_parser: Reduce test silliness
    Running 100% of the base parser tests at compile time was a fun
    experiment, but the tests looking more standard and being able to run
    them under Clang as well outweighs the meme-value of it.

--- util/base_parser_test.cpp
+++ util/base_parser_test.cpp
@@ -9,39 +9,28 @@
 using etest::expect;
 using util::BaseParser;
 
-namespace {
-
-template<bool B>
-bool static_test() {
-    static_assert(B);
-    return B;
-}
-
-} // namespace
-
 int main() {
     etest::test("peek", [] {
         constexpr auto abcd = BaseParser("abcd");
-        expect(static_test<abcd.peek() == 'a'>());
-        expect(static_test<abcd.peek(2) == "ab">());
-        expect(static_test<abcd.peek(3) == "abc">());
-        expect(static_test<abcd.peek(4) == "abcd">());
-        expect(static_test<BaseParser(" ").peek() == ' '>());
+        expect(abcd.peek() == 'a');
+        expect(abcd.peek(2) == "ab");
+        expect(abcd.peek(3) == "abc");
+        expect(abcd.peek(4) == "abcd");
+        expect(BaseParser(" ").peek() == ' ');
     });
 
     etest::test("starts_with", [] {
         constexpr auto abcd = BaseParser("abcd");
-        expect(static_test<!abcd.starts_with("hello")>());
-        expect(static_test<abcd.starts_with("ab")>());
-        expect(static_test<abcd.starts_with("abcd")>());
+        expect(!abcd.starts_with("hello"));
+        expect(abcd.starts_with("ab"));
+        expect(abcd.starts_with("abcd"));
     });
 
-#ifndef __clang__ // Clang doesn't yet support lambdas in templates.
     etest::test("is_eof, advance", [] {
         constexpr auto abcd = BaseParser("abcd");
-        expect(static_test<!abcd.is_eof()>());
-        expect(static_test<BaseParser("").is_eof()>());
-        expect(static_test<[] {
+        expect(!abcd.is_eof());
+        expect(BaseParser("").is_eof());
+        expect([] {
             auto p = BaseParser("abcd");
             p.advance(3);
             if (p.is_eof()) {
@@ -49,11 +38,11 @@ int main() {
             }
             p.advance(1);
             return p.is_eof();
-        }()>());
+        }());
     });
 
     etest::test("consume_char", [] {
-        expect(static_test<[] {
+        expect([] {
             auto p = BaseParser("abcd");
             if (p.consume_char() != 'a') {
                 return false;
@@ -68,11 +57,11 @@ int main() {
                 return false;
             }
             return true;
-        }()>());
+        }());
     });
 
     etest::test("consume_while", [] {
-        expect(static_test<[] {
+        expect([] {
             auto p = BaseParser("abcd");
             if (p.consume_while([](char c) { return c != 'c'; }) != "ab") {
                 return false;
@@ -81,11 +70,11 @@ int main() {
                 return false;
             }
             return true;
-        }()>());
+        }());
     });
 
     etest::test("skip_whitespace, consume_char", [] {
-        expect(static_test<[] {
+        expect([] {
             auto p = BaseParser("      \t       \n         h          \n\n\ni");
             p.skip_whitespace();
             if (p.consume_char() != 'h') {
@@ -96,7 +85,6 @@ int main() {
                 return false;
             }
             return true;
-        }()>());
+        }());
     });
-#endif
 }
