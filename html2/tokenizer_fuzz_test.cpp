7cc79365032afc119da2f07e3b70c0256b71c60d
Robin Linden <dev@robinlinden.eu>
2022-09-22 00:27:40 +0200

    html2: Set up a fuzz test for the tokenizer
    This mostly serves as an example for further work on fuzzing things as
    the tokenizer is still too incomplete for this to be useful.

---
+++ html2/tokenizer_fuzz_test.cpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2022 Robin Lindén <dev@robinlinden.eu>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+
+#include "html2/tokenizer.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <optional>
+#include <string_view>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size);
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size) {
+    html2::Tokenizer{std::string_view{reinterpret_cast<char const *>(data), size},
+            [](html2::Tokenizer &tokenizer, html2::Token &&token) {
+                if (auto const *start_tag = std::get_if<html2::StartTagToken>(&token)) {
+                    if (start_tag->tag_name == "script") {
+                        tokenizer.set_state(html2::State::ScriptData);
+                    }
+                }
+            }}
+            .run();
+    return 0;
+}
