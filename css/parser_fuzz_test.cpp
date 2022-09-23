dd5b6876eb4a816c752a116079782f91832a723d
Robin Linden <dev@robinlinden.eu>
2022-09-23 02:41:57 +0200

    css: Add a fuzz test for css parsing

---
+++ css/parser_fuzz_test.cpp
@@ -0,0 +1,17 @@
+// SPDX-FileCopyrightText: 2022 Robin Lindén <dev@robinlinden.eu>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+
+#include "css/parse.h"
+
+#include <cstddef>
+#include <cstdint>
+#include <string_view>
+#include <tuple>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size);
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size) {
+    std::ignore = css::parse(std::string_view{reinterpret_cast<char const *>(data), size});
+    return 0;
+}
