a238badc5686de141c84bf525092a377e279872c
Robin Linden <dev@robinlinden.eu>
2023-02-17 22:57:41 +0100

    wasm: Support parsing leb128 into any of the unsigned integral types

--- wasm/leb128.h
+++ wasm/leb128.h
@@ -6,26 +6,36 @@
 #define WASM_LEB128_H_
 
 #include <cmath>
+#include <concepts>
 #include <cstdint>
 #include <istream>
 #include <optional>
 
 namespace wasm {
 
-// https://en.wikipedia.org/wiki/LEB128#Decode_unsigned_integer
 // https://webassembly.github.io/spec/core/bikeshed/#binary-int
+template<typename T>
+requires std::integral<T>
 struct Leb128 {
-    static std::optional<std::uint32_t> decode_from(std::istream &&is) { return decode_from(is); }
-    static std::optional<std::uint32_t> decode_from(std::istream &is) {
-        std::uint32_t result{};
+    // clang-format-14 adds a newline between the braces and clang-format-15
+    // removes it. This comment keeps the formatting stable until we update to
+    // clang-format-15.
+};
+
+// https://en.wikipedia.org/wiki/LEB128#Decode_unsigned_integer
+template<std::unsigned_integral T>
+struct Leb128<T> {
+    static std::optional<T> decode_from(std::istream &&is) { return decode_from(is); }
+    static std::optional<T> decode_from(std::istream &is) {
+        T result{};
         std::uint8_t shift{};
-        for (int i = 0; i < std::ceil(sizeof(std::uint32_t) * 8 / 7.f); ++i) {
+        for (int i = 0; i < std::ceil(sizeof(T) * 8 / 7.f); ++i) {
             std::uint8_t byte{};
             if (!is.read(reinterpret_cast<char *>(&byte), sizeof(byte))) {
                 return std::nullopt;
             }
 
-            result |= (byte & 0b0111'1111) << shift;
+            result |= static_cast<T>(byte & 0b0111'1111) << shift;
             if (!(byte & 0b1000'0000)) {
                 return result;
             }
--- wasm/leb128_test.cpp
+++ wasm/leb128_test.cpp
@@ -15,23 +15,24 @@ using wasm::Leb128;
 
 int main() {
     etest::test("decode unsigned", [] {
-        expect_eq(Leb128::decode_from(std::stringstream{"\0"s}), std::uint32_t{0});
-        expect_eq(Leb128::decode_from(std::stringstream{"\1"}), std::uint32_t{1});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x3f"}), std::uint32_t{63});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x40"}), std::uint32_t{64});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x7f"}), std::uint32_t{0x7f});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x80\x01"}), std::uint32_t{0x80});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x80\x02"}), std::uint32_t{0x100});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x80\x7f"}), std::uint32_t{16256});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x81\x01"}), std::uint32_t{0x81});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x81\x02"}), std::uint32_t{0x101});
-        expect_eq(Leb128::decode_from(std::stringstream{"\x90\x01"}), std::uint32_t{0x90});
-        expect_eq(Leb128::decode_from(std::stringstream{"\xff\x01"}), std::uint32_t{0xff});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\0"s}), std::uint32_t{0});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\1"}), std::uint32_t{1});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x3f"}), std::uint32_t{63});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x40"}), std::uint32_t{64});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x7f"}), std::uint32_t{0x7f});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x80\x01"}), std::uint32_t{0x80});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x80\x02"}), std::uint32_t{0x100});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x80\x7f"}), std::uint32_t{16256});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x81\x01"}), std::uint32_t{0x81});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x81\x02"}), std::uint32_t{0x101});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x90\x01"}), std::uint32_t{0x90});
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\xff\x01"}), std::uint32_t{0xff});
+        expect_eq(Leb128<std::uint64_t>::decode_from(std::stringstream{"\x80\xc1\x80\x80\x10"}), std::uint64_t{4294975616});
 
         // Missing termination.
-        expect_eq(Leb128::decode_from(std::stringstream{"\x80"}), std::nullopt);
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x80"}), std::nullopt);
         // Too many bytes with no termination.
-        expect_eq(Leb128::decode_from(std::stringstream{"\x80\x80\x80\x80\x80\x80"}), std::nullopt);
+        expect_eq(Leb128<std::uint32_t>::decode_from(std::stringstream{"\x80\x80\x80\x80\x80\x80"}), std::nullopt);
     });
 
     return etest::run_all_tests();
