9566ac2fb475fb21dece3ae1a243b24a0c75e375
Robin Linden <dev@robinlinden.eu>
2023-02-19 01:26:29 +0100

    wasm: Dump types in the example

--- wasm/wasm_example.cpp
+++ wasm/wasm_example.cpp
@@ -4,8 +4,43 @@
 
 #include "wasm/wasm.h"
 
+#include <algorithm>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+
+namespace wasm {
+std::ostream &operator<<(std::ostream &, wasm::ValueType);
+std::ostream &operator<<(std::ostream &os, wasm::ValueType type) {
+    switch (type) {
+        case ValueType::Int32:
+            os << "i32";
+            break;
+        case ValueType::Int64:
+            os << "i64";
+            break;
+        case ValueType::Float32:
+            os << "f32";
+            break;
+        case ValueType::Float64:
+            os << "f64";
+            break;
+        case ValueType::Vector128:
+            os << "v128";
+            break;
+        case ValueType::FunctionReference:
+            os << "funcref";
+            break;
+        case ValueType::ExternReference:
+            os << "externref";
+            break;
+        default:
+            std::abort();
+    }
+    return os;
+}
+} // namespace wasm
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -27,6 +62,26 @@ int main(int argc, char **argv) {
 
     for (auto const &section : module->sections) {
         std::cout << static_cast<int>(section.id) << ": " << section.content.size() << '\n';
+    }
+
+    if (auto const &type_section = module->type_section()) {
+        // Prints a list of wasm::ValueType separated by commas.
+        // https://en.cppreference.com/w/cpp/experimental/ostream_joiner soon, I hope.
+        auto print_values = [](auto const &values) {
+            if (!values.empty()) {
+                std::copy_n(begin(values), size(values) - 1, std::ostream_iterator<wasm::ValueType>(std::cout, ","));
+                std::copy_n(end(values) - 1, 1, std::ostream_iterator<wasm::ValueType>(std::cout));
+            }
+        };
+
+        std::cout << '\n';
+        for (auto const &type : type_section->types) {
+            std::cout << '(';
+            print_values(type.parameters);
+            std::cout << ") -> (";
+            print_values(type.results);
+            std::cout << ")\n";
+        }
     }
 
     if (auto const &export_section = module->export_section()) {
