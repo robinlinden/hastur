c36c1789bc5f874932b398af7230b5998dbc6b35
Robin Linden <dev@robinlinden.eu>
2023-05-17 23:44:58 +0200

    layout: Add a helper for testing property getters

--- layout/layout_property_test.cpp
+++ layout/layout_property_test.cpp
@@ -4,10 +4,43 @@
 
 #include "layout/layout.h"
 
+#include "etest/cxx_compat.h"
 #include "etest/etest.h"
+
+#include <optional>
+#include <string>
+#include <utility>
+#include <vector>
 
 using namespace std::literals;
 using etest::expect_eq;
+
+namespace {
+template<css::PropertyId IdT>
+void expect_property_eq(std::optional<std::string> value,
+        auto expected,
+        std::vector<std::pair<css::PropertyId, std::string>> extra_properties = {},
+        etest::source_location const &loc = etest::source_location::current()) {
+    dom::Node dom_node = dom::Element{"dummy"s};
+    style::StyledNode styled_node{
+            .node = dom_node,
+            .properties = std::move(extra_properties),
+            .children = {},
+    };
+
+    if (value) {
+        styled_node.properties.push_back({IdT, *std::move(value)});
+    }
+
+    auto layout = layout::create_layout(styled_node, 123);
+    if (!layout) {
+        etest::expect(false, std::nullopt, loc);
+        return;
+    }
+
+    etest::expect_eq(layout->get_property<IdT>(), expected, std::nullopt, loc);
+};
+} // namespace
 
 int main() {
     etest::test("border radius", [] {
