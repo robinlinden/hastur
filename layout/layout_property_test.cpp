9bb11535e08fb444c0e9d19cd767d81ad5d71cb7
Robin Linden <dev@robinlinden.eu>
2023-05-17 23:57:35 +0200

    layout: Migrate test to the property test helper

--- layout/layout_property_test.cpp
+++ layout/layout_property_test.cpp
@@ -13,7 +13,6 @@
 #include <vector>
 
 using namespace std::literals;
-using etest::expect_eq;
 
 namespace {
 template<css::PropertyId IdT>
@@ -43,22 +42,12 @@ void expect_property_eq(std::optional<std::string> value,
 } // namespace
 
 int main() {
+    using enum css::PropertyId;
     etest::test("border radius", [] {
-        dom::Node html_node = dom::Element{"html"s};
-        style::StyledNode styled_node{
-                .node = html_node,
-                .properties{
-                        {css::PropertyId::FontSize, "30px"},
-                        {css::PropertyId::BorderTopLeftRadius, "2em"},
-                        {css::PropertyId::BorderBottomRightRadius, "10px/3em"},
-                },
-        };
-        auto layout = layout::create_layout(styled_node, 123).value();
-
-        expect_eq(layout.get_property<css::PropertyId::BorderTopLeftRadius>(), std::pair{60, 60});
-        expect_eq(layout.get_property<css::PropertyId::BorderTopRightRadius>(), std::pair{0, 0});
-        expect_eq(layout.get_property<css::PropertyId::BorderBottomLeftRadius>(), std::pair{0, 0});
-        expect_eq(layout.get_property<css::PropertyId::BorderBottomRightRadius>(), std::pair{10, 90});
+        expect_property_eq<BorderTopLeftRadius>("2em", std::pair{60, 60}, {{FontSize, "30px"}});
+        expect_property_eq<BorderTopRightRadius>(std::nullopt, std::pair{0, 0});
+        expect_property_eq<BorderBottomLeftRadius>(std::nullopt, std::pair{0, 0});
+        expect_property_eq<BorderBottomRightRadius>("10px/3em", std::pair{10, 90}, {{FontSize, "30px"}});
     });
 
     return etest::run_all_tests();
