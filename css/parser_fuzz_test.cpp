c77a089485409505de5396c0eb6c11e31aaaa63d
Robin Linden <dev@robinlinden.eu>
2023-05-30 22:57:28 +0200

    css: Fix fuzz test compilation

--- css/parser_fuzz_test.cpp
+++ css/parser_fuzz_test.cpp
@@ -2,14 +2,14 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
-#include "css/parse.h"
+#include "css/parser.h"
 
 #include <stddef.h> // NOLINT
 #include <stdint.h> // NOLINT
 #include <string_view>
 #include <tuple>
 
-extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size);
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size); // NOLINT
 
 extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size) {
     std::ignore = css::parse(std::string_view{reinterpret_cast<char const *>(data), size});
