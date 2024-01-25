robinlinden/hastur

commit 042be7e73fea6d6bcc4990fc367d61c04030e27a

deps/imgui-sfml: Patch out the system GL dependency in favour of glad

WORKSPACE
@@ -163,6 +163,11 @@ http_archive(
 http_archive(
     name = "imgui-sfml",  # MIT
     build_file = "//third_party:imgui-sfml.BUILD",
+    patch_cmds = [
+        # Use glad for OpenGL instead of the system OpenGL headers.
+        "sed -i'' -e /OpenGL.hpp/d imgui-SFML.cpp",
+        "sed -i'' -e '4i\\\n#include <glad/gl.h>' imgui-SFML.cpp",
+    ],
     sha256 = "b1195ca1210dd46c8049cfc8cae7f31cd34f1591da7de1c56297b277ac9c5cc0",
     strip_prefix = "imgui-sfml-2.6",
     url = "https://github.com/eliasdaler/imgui-sfml/archive/v2.6.tar.gz",

third_party/imgui-sfml.BUILD
@@ -1,18 +1,21 @@
 load("@rules_cc//cc:defs.bzl", "cc_library")
 
+# TODO(robinlinden): False positive layering check error. Related to glad being
+# a local_repository? See: bazel-out/k8-fastbuild/bin/external/glad/glad.cppmap
+#
+# external/imgui-sfml/imgui-SFML.cpp:4:10: error: module imgui-sfml//:imgui-sfml
+# does not depend on a module exporting 'glad/gl.h'
+package(features = ["-layering_check"])
+
 cc_library(
     name = "imgui-sfml",
     srcs = ["imgui-SFML.cpp"],
     hdrs = glob(["*.h"]),
     defines = ["IMGUI_SFML_SHARED_LIB=0"],
     includes = ["."],
-    linkopts = select({
-        "@platforms//os:linux": ["-lGL"],
-        "@platforms//os:macos": ["-lGL"],
-        "@platforms//os:windows": ["-DEFAULTLIB:opengl32"],
-    }),
     visibility = ["//visibility:public"],
     deps = [
+        "@glad",
         "@imgui",
         "@sfml//:graphics",
         "@sfml//:system",

commit 68dade0cd91d5fc194f00c0f439fe34361b6355b

deps/imgui-sfml: Fix crash due to missing glad-init
This broke in 042be7e73fea6d6bcc4990fc367d61c04030e27a when switching to
dynamically loading OpenGL w/ glad.

WORKSPACE
@@ -167,6 +167,7 @@ http_archive(
         # Use glad for OpenGL instead of the system OpenGL headers.
         "sed -i'' -e /OpenGL.hpp/d imgui-SFML.cpp",
         "sed -i'' -e '4i\\\n#include <glad/gl.h>' imgui-SFML.cpp",
+        "sed -i'' -e '277i\\\n\\\tif (gladLoaderLoadGL() == 0) return false;' imgui-SFML.cpp",
     ],
     sha256 = "b1195ca1210dd46c8049cfc8cae7f31cd34f1591da7de1c56297b277ac9c5cc0",
     strip_prefix = "imgui-sfml-2.6",