f12b1ef967ea4c3af27009ec9c91eb047b2f7ba8
Robin Linden <dev@robinlinden.eu>
2024-04-18 22:11:40 +0200

    img/test: Set up fuzz tests for all image types

---
+++ img/jpeg_fuzz_test.cpp
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2024 Robin Lindén <dev@robinlinden.eu>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+
+#include "img/jpeg.h"
+
+#include <sstream>
+#include <stddef.h> // NOLINT
+#include <stdint.h> // NOLINT
+#include <string>
+#include <tuple>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size); // NOLINT
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t const *data, size_t size) {
+    std::ignore = img::Jpeg::thumbnail_from( //
+            std::stringstream{std::string{reinterpret_cast<char const *>(data), size}});
+    return 0;
+}
