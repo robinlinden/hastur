robinlinden/hastur

commit 9e3b54318b6f7a7547183e942a72e3a8ef95bbb4

build: Enable fuzz tests on macOS as well

bzl/copts.bzl
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2022-2023 Robin Lindén <dev@robinlinden.eu>
+# SPDX-FileCopyrightText: 2022-2024 Robin Lindén <dev@robinlinden.eu>
 #
 # SPDX-License-Identifier: BSD-2-Clause
 
@@ -49,6 +49,9 @@ HASTUR_COPTS = select({
 
 # C++ fuzzing requires a Clang compiler: https://github.com/bazelbuild/rules_fuzzing#prerequisites
 HASTUR_FUZZ_PLATFORMS = select({
-    "@rules_cc//cc/compiler:clang": ["@platforms//os:linux"],
+    "@rules_cc//cc/compiler:clang": [
+        "@platforms//os:linux",
+        "@platforms//os:macos",
+    ],
     "//conditions:default": ["@platforms//:incompatible"],
 })

commit 9ae2c4d5f477d0d46b776e7275c767681286608f

build: Fix broken fuzz test platform selection
I broke this in 9e3b54318b6f7a7547183e942a72e3a8ef95bbb4, and since we
don't explicitly run any fuzz tests in CI, they were just quietly
disabled.

bzl/BUILD
@@ -3,3 +3,13 @@ filegroup(
     srcs = ["run_xfail_test"],
     visibility = ["//visibility:public"],
 )
+
+alias(
+    name = "linux_or_macos",
+    actual = select({
+        "@platforms//os:linux": "@platforms//os:linux",
+        "@platforms//os:macos": "@platforms//os:macos",
+        "//conditions:default": "@platforms//:incompatible",
+    }),
+    visibility = ["//visibility:public"],
+)

bzl/copts.bzl
@@ -45,9 +45,6 @@ HASTUR_COPTS = select({
 
 # C++ fuzzing requires a Clang compiler: https://github.com/bazelbuild/rules_fuzzing#prerequisites
 HASTUR_FUZZ_PLATFORMS = select({
-    "@rules_cc//cc/compiler:clang": [
-        "@platforms//os:linux",
-        "@platforms//os:macos",
-    ],
+    "@rules_cc//cc/compiler:clang": ["//bzl:linux_or_macos"],
     "//conditions:default": ["@platforms//:incompatible"],
 })