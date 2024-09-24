5f428677e82826edaccc88c571be0a5cdfb35f1e
Robin Linden <dev@robinlinden.eu>
2024-09-24 21:45:01 +0200

    util/from_chars/test: Migrate to etest2

--- util/from_chars_test.cpp
+++ util/from_chars_test.cpp
@@ -1,53 +1,54 @@
-// SPDX-FileCopyrightText: 2022 Robin Lindén <dev@robinlinden.eu>
+// SPDX-FileCopyrightText: 2022-2024 Robin Lindén <dev@robinlinden.eu>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
 #include "util/from_chars.h"
 
-#include "etest/etest.h"
+#include "etest/etest2.h"
 
 #include <string_view>
 #include <system_error>
 
 using namespace std::literals;
-using etest::expect_eq;
 
 int main() {
-    etest::test("success", [] {
+    etest::Suite s;
+
+    s.add_test("success", [](etest::IActions &a) {
         auto from = "100.5"sv;
         float v{};
         auto res = util::from_chars(from.data(), from.data() + from.size(), v);
-        expect_eq(res, util::from_chars_result{from.data() + from.size(), std::errc{}});
-        expect_eq(v, 100.5f);
+        a.expect_eq(res, util::from_chars_result{from.data() + from.size(), std::errc{}});
+        a.expect_eq(v, 100.5f);
     });
 
-    etest::test("success, negative", [] {
+    s.add_test("success, negative", [](etest::IActions &a) {
         auto from = "-100.5"sv;
         float v{};
         auto res = util::from_chars(from.data(), from.data() + from.size(), v);
-        expect_eq(res, util::from_chars_result{from.data() + from.size(), std::errc{}});
-        expect_eq(v, -100.5f);
+        a.expect_eq(res, util::from_chars_result{from.data() + from.size(), std::errc{}});
+        a.expect_eq(v, -100.5f);
     });
 
-    etest::test("failure, out of range", [] {
+    s.add_test("failure, out of range", [](etest::IActions &a) {
         auto from = "1e100000"sv;
         float v{};
         auto res = util::from_chars(from.data(), from.data() + from.size(), v);
-        expect_eq(res, util::from_chars_result{from.data() + from.size(), std::errc::result_out_of_range});
+        a.expect_eq(res, util::from_chars_result{from.data() + from.size(), std::errc::result_out_of_range});
 #ifndef _MSC_VER
         // Microsoft's STL sets v to HUGE_VALF when ERANGE occurs.
         // See: https://en.cppreference.com/w/cpp/utility/from_chars#Return_value
-        expect_eq(v, 0.f);
+        a.expect_eq(v, 0.f);
 #endif
     });
 
-    etest::test("failure, not a float", [] {
+    s.add_test("failure, not a float", [](etest::IActions &a) {
         auto from = "abcd"sv;
         float v{};
         auto res = util::from_chars(from.data(), from.data() + from.size(), v);
-        expect_eq(res, util::from_chars_result{from.data(), std::errc::invalid_argument});
-        expect_eq(v, 0.f);
+        a.expect_eq(res, util::from_chars_result{from.data(), std::errc::invalid_argument});
+        a.expect_eq(v, 0.f);
     });
 
-    return etest::run_all_tests();
+    return s.run();
 }
