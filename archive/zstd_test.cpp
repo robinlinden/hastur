82fcaa94007b517440ac5a62227ebaeb373f3c2f
Robin Linden <dev@robinlinden.eu>
2025-01-27 02:02:09 +0100

    archive/zstd: Make the max output size configurable

--- archive/zstd_test.cpp
+++ archive/zstd_test.cpp
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2024 David Zero <zero-one@zer0-one.net>
-// SPDX-FileCopyrightText: 2024 Robin Lindén <dev@robinlinden.eu>
+// SPDX-FileCopyrightText: 2024-2025 Robin Lindén <dev@robinlinden.eu>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
@@ -14,12 +14,51 @@
 #include <cstdint>
 #include <span>
 #include <string>
+#include <string_view>
 #include <vector>
 
 namespace {
 std::span<std::byte const> as_bytes(std::span<std::uint8_t const> s) {
     return {reinterpret_cast<std::byte const *>(s.data()), s.size()};
 }
+
+// "This is a test string\n"
+constexpr auto kSuccessTestString = std::to_array<std::uint8_t>({0x28,
+        0xb5,
+        0x2f,
+        0xfd,
+        0x04,
+        0x00,
+        0xb1,
+        0x00,
+        0x00,
+        0x54,
+        0x68,
+        0x69,
+        0x73,
+        0x20,
+        0x69,
+        0x73,
+        0x20,
+        0x61,
+        0x20,
+        0x74,
+        0x65,
+        0x73,
+        0x74,
+        0x20,
+        0x73,
+        0x74,
+        0x72,
+        0x69,
+        0x6e,
+        0x67,
+        0x0a,
+        0xd8,
+        0x6a,
+        0x8c,
+        0x62});
+
 } // namespace
 
 int main() {
@@ -28,46 +67,24 @@ int main() {
     using namespace archive;
 
     s.add_test("trivial decode", [](etest::IActions &a) {
-        constexpr auto kCompress = std::to_array<std::uint8_t>({0x28,
-                0xb5,
-                0x2f,
-                0xfd,
-                0x04,
-                0x00,
-                0xb1,
-                0x00,
-                0x00,
-                0x54,
-                0x68,
-                0x69,
-                0x73,
-                0x20,
-                0x69,
-                0x73,
-                0x20,
-                0x61,
-                0x20,
-                0x74,
-                0x65,
-                0x73,
-                0x74,
-                0x20,
-                0x73,
-                0x74,
-                0x72,
-                0x69,
-                0x6e,
-                0x67,
-                0x0a,
-                0xd8,
-                0x6a,
-                0x8c,
-                0x62});
-
-        auto ret = zstd_decode(as_bytes(kCompress));
+        auto ret = zstd_decode(as_bytes(kSuccessTestString));
 
         a.expect(ret.has_value());
         a.expect_eq(std::string(reinterpret_cast<char const *>(ret->data()), ret->size()), "This is a test string\n");
+    });
+
+    s.add_test("output too large", [](etest::IActions &a) {
+        ZstdDecoder decoder;
+
+        decoder.set_max_output_length(21);
+        auto ret = decoder.decode(as_bytes(kSuccessTestString));
+        a.expect_eq(ret, tl::unexpected{ZstdError::MaximumOutputLengthExceeded});
+
+        decoder.set_max_output_length(22);
+        ret = decoder.decode(as_bytes(kSuccessTestString));
+        a.require(ret.has_value());
+        a.expect_eq(
+                std::string_view{reinterpret_cast<char const *>(ret->data()), ret->size()}, "This is a test string\n");
     });
 
     s.add_test("empty input", [](etest::IActions &a) {
