0ba5e904737c4c0fd0f6549215d06fb8ab52cc57
Robin Linden <dev@robinlinden.eu>
2025-01-27 01:19:26 +0100

    archive/brotli: Make the max output size configurable
    The output-size check was moved later to ensure that we don't return
    outputs that can be up to a chunk size larger than the specified max.

--- archive/brotli.h
+++ archive/brotli.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2024 David Zero <zero-one@zer0-one.net>
+// SPDX-FileCopyrightText: 2025 Robin Lindén <dev@robinlinden.eu>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
@@ -25,7 +26,19 @@ enum class BrotliError : std::uint8_t {
 
 std::string_view to_string(BrotliError);
 
-tl::expected<std::vector<std::byte>, BrotliError> brotli_decode(std::span<std::byte const>);
+class BrotliDecoder {
+public:
+    tl::expected<std::vector<std::byte>, BrotliError> decode(std::span<std::byte const>) const;
+
+    void set_max_output_length(std::size_t length) { max_output_length_ = length; }
+
+private:
+    std::size_t max_output_length_ = std::size_t{1024} * 1024 * 1024;
+};
+
+inline tl::expected<std::vector<std::byte>, BrotliError> brotli_decode(std::span<std::byte const> input) {
+    return BrotliDecoder{}.decode(input);
+}
 
 } // namespace archive
 
