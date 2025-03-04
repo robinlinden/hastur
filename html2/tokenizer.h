7be1303b2d4f4ed28ea2652325d8f2e9323d187c
Robin Linden <dev@robinlinden.eu>
2025-03-04 22:52:35 +0100

    html2: Support stringifying parse errors

--- html2/tokenizer.h
+++ html2/tokenizer.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2021-2024 Robin Lindén <dev@robinlinden.eu>
+// SPDX-FileCopyrightText: 2021-2025 Robin Lindén <dev@robinlinden.eu>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
@@ -149,6 +149,103 @@ enum class ParseError : std::uint8_t {
     UnexpectedSolidusInTag,
     UnknownNamedCharacterReference,
 };
+
+constexpr std::string_view to_string(ParseError e) {
+    switch (e) {
+        case ParseError::AbruptClosingOfEmptyComment:
+            return "AbruptClosingOfEmptyComment";
+        case ParseError::AbruptDoctypePublicIdentifier:
+            return "AbruptDoctypePublicIdentifier";
+        case ParseError::AbruptDoctypeSystemIdentifier:
+            return "AbruptDoctypeSystemIdentifier";
+        case ParseError::AbsenceOfDigitsInNumericCharacterReference:
+            return "AbsenceOfDigitsInNumericCharacterReference";
+        case ParseError::CdataInHtmlContent:
+            return "CdataInHtmlContent";
+        case ParseError::CharacterReferenceOutsideUnicodeRange:
+            return "CharacterReferenceOutsideUnicodeRange";
+        case ParseError::ControlCharacterReference:
+            return "ControlCharacterReference";
+        case ParseError::DuplicateAttribute:
+            return "DuplicateAttribute";
+        case ParseError::EndTagWithAttributes:
+            return "EndTagWithAttributes";
+        case ParseError::EndTagWithTrailingSolidus:
+            return "EndTagWithTrailingSolidus";
+        case ParseError::EofBeforeTagName:
+            return "EofBeforeTagName";
+        case ParseError::EofInCdata:
+            return "EofInCdata";
+        case ParseError::EofInComment:
+            return "EofInComment";
+        case ParseError::EofInDoctype:
+            return "EofInDoctype";
+        case ParseError::EofInScriptHtmlCommentLikeText:
+            return "EofInScriptHtmlCommentLikeText";
+        case ParseError::EofInTag:
+            return "EofInTag";
+        case ParseError::IncorrectlyClosedComment:
+            return "IncorrectlyClosedComment";
+        case ParseError::IncorrectlyOpenedComment:
+            return "IncorrectlyOpenedComment";
+        case ParseError::InvalidCharacterSequenceAfterDoctypeName:
+            return "InvalidCharacterSequenceAfterDoctypeName";
+        case ParseError::InvalidFirstCharacterOfTagName:
+            return "InvalidFirstCharacterOfTagName";
+        case ParseError::MissingAttributeValue:
+            return "MissingAttributeValue";
+        case ParseError::MissingDoctypeName:
+            return "MissingDoctypeName";
+        case ParseError::MissingDoctypePublicIdentifier:
+            return "MissingDoctypePublicIdentifier";
+        case ParseError::MissingDoctypeSystemIdentifier:
+            return "MissingDoctypeSystemIdentifier";
+        case ParseError::MissingEndTagName:
+            return "MissingEndTagName";
+        case ParseError::MissingQuoteBeforeDoctypePublicIdentifier:
+            return "MissingQuoteBeforeDoctypePublicIdentifier";
+        case ParseError::MissingQuoteBeforeDoctypeSystemIdentifier:
+            return "MissingQuoteBeforeDoctypeSystemIdentifier";
+        case ParseError::MissingSemicolonAfterCharacterReference:
+            return "MissingSemicolonAfterCharacterReference";
+        case ParseError::MissingWhitespaceAfterDoctypePublicKeyword:
+            return "MissingWhitespaceAfterDoctypePublicKeyword";
+        case ParseError::MissingWhitespaceAfterDoctypeSystemKeyword:
+            return "MissingWhitespaceAfterDoctypeSystemKeyword";
+        case ParseError::MissingWhitespaceBeforeDoctypeName:
+            return "MissingWhitespaceBeforeDoctypeName";
+        case ParseError::MissingWhitespaceBetweenAttributes:
+            return "MissingWhitespaceBetweenAttributes";
+        case ParseError::MissingWhitespaceBetweenDoctypePublicAndSystemIdentifiers:
+            return "MissingWhitespaceBetweenDoctypePublicAndSystemIdentifiers";
+        case ParseError::NestedComment:
+            return "NestedComment";
+        case ParseError::NoncharacterCharacterReference:
+            return "NoncharacterCharacterReference";
+        case ParseError::NullCharacterReference:
+            return "NullCharacterReference";
+        case ParseError::SurrogateCharacterReference:
+            return "SurrogateCharacterReference";
+        case ParseError::UnexpectedCharacterAfterDoctypeSystemIdentifier:
+            return "UnexpectedCharacterAfterDoctypeSystemIdentifier";
+        case ParseError::UnexpectedCharacterInAttributeName:
+            return "UnexpectedCharacterInAttributeName";
+        case ParseError::UnexpectedCharacterInUnquotedAttributeValue:
+            return "UnexpectedCharacterInUnquotedAttributeValue";
+        case ParseError::UnexpectedEqualsSignBeforeAttributeName:
+            return "UnexpectedEqualsSignBeforeAttributeName";
+        case ParseError::UnexpectedNullCharacter:
+            return "UnexpectedNullCharacter";
+        case ParseError::UnexpectedQuestionMarkInsteadOfTagName:
+            return "UnexpectedQuestionMarkInsteadOfTagName";
+        case ParseError::UnexpectedSolidusInTag:
+            return "UnexpectedSolidusInTag";
+        case ParseError::UnknownNamedCharacterReference:
+            return "UnknownNamedCharacterReference";
+    }
+
+    return "Unknown error";
+}
 
 struct SourceLocation {
     int line{};
