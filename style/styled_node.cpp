aa2d57e5649fb676231b852ffb304886bc8c9b9e
Robin Linden <dev@robinlinden.eu>
2025-03-21 02:02:35 +0100

    style: Don't log successful var-fallback lookups

--- style/styled_node.cpp
+++ style/styled_node.cpp
@@ -297,9 +297,9 @@ std::optional<std::string_view> StyledNode::resolve_variable(std::string_view va
         }
 
         if (!prop) {
-            spdlog::info("No matching variable for custom property '{}'", var_name);
             fallback = util::trim(fallback);
             if (fallback.empty()) {
+                spdlog::warn("Unresolved variable w/ no fallback '{}'", var_name);
                 break;
             }
 
