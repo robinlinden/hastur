cccfc2b4c7bbb8914208dc8246fdc12ac39c4a4d
Robin Linden <dev@robinlinden.eu>
2025-04-27 01:06:01 +0200

    protocol/test: Simplify the response test
    No need to explicitly turn these into string_views.

--- protocol/response_test.cpp
+++ protocol/response_test.cpp
@@ -7,22 +7,19 @@
 
 #include "etest/etest2.h"
 
-#include <string_view>
 #include <type_traits>
-
-using namespace std::string_view_literals;
 
 int main() {
     etest::Suite s;
 
     s.add_test("ErrorCode, to_string", [](etest::IActions &a) {
         using protocol::ErrorCode;
-        a.expect_eq(to_string(ErrorCode::Unresolved), "Unresolved"sv);
-        a.expect_eq(to_string(ErrorCode::Unhandled), "Unhandled"sv);
-        a.expect_eq(to_string(ErrorCode::InvalidResponse), "InvalidResponse"sv);
-        a.expect_eq(to_string(ErrorCode::RedirectLimit), "RedirectLimit"sv);
+        a.expect_eq(to_string(ErrorCode::Unresolved), "Unresolved");
+        a.expect_eq(to_string(ErrorCode::Unhandled), "Unhandled");
+        a.expect_eq(to_string(ErrorCode::InvalidResponse), "InvalidResponse");
+        a.expect_eq(to_string(ErrorCode::RedirectLimit), "RedirectLimit");
         // NOLINTNEXTLINE(clang-analyzer-optin.core.EnumCastOutOfRange)
-        a.expect_eq(to_string(static_cast<ErrorCode>(std::underlying_type_t<ErrorCode>{20})), "Unknown"sv);
+        a.expect_eq(to_string(static_cast<ErrorCode>(std::underlying_type_t<ErrorCode>{20})), "Unknown");
     });
 
     s.add_test("to_string(Headers)", [](etest::IActions &a) {
