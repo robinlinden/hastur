ea1b7272af52d5f19cf2b4aed1738d6f119ed86c
Robin Linden <dev@robinlinden.eu>
2025-08-15 19:03:59 +0200

    tool: Set up a tool for embedding resources in a nicer-than-xxd way
    This will go away once we target compilers w/ support for `#embed`, but
    I feel this is a decent intermediate step as this is very little code,
    allows us to treat the resources as compile-time constants instead of
    mutable data, and doesn't require any namespace hacks where we include
    the resources.

---
+++ tool/embed.cpp
@@ -0,0 +1,60 @@
+// SPDX-FileCopyrightText: 2025 Robin Lindén <dev@robinlinden.eu>
+//
+// SPDX-License-Identifier: BSD-2-Clause
+
+#include "util/arg_parser.h"
+
+#include <array>
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+int main(int argc, char **argv) {
+    std::string name;
+    std::string input_file;
+
+    if (auto res = util::ArgParser{}.argument("--name", name).positional(input_file).parse(argc, argv); !res) {
+        std::cerr << "Error: " << res.error().message << '\n';
+        return 1;
+    }
+
+    if (input_file.empty() || name.empty()) {
+        std::string_view program_name = argv[0] != nullptr ? argv[0] : "<bin>";
+        std::cerr << "Usage: " << program_name << " --name <name> <input_file>\n";
+        return 1;
+    }
+
+    std::ifstream file(input_file, std::ios::binary);
+    if (!file) {
+        std::cerr << "Error: Could not open file '" << input_file << "'\n";
+        return 1;
+    }
+
+    std::cout << "#ifndef HST_GENERATED_" << name << "_H\n";
+    std::cout << "#define HST_GENERATED_" << name << "_H\n";
+
+    std::cout << "#include <array>\n";
+    std::cout << "#include <string_view>\n";
+    std::cout << "constexpr auto " << name << "Bytes = std::to_array<char>({\n";
+    std::array<char, 1024> data{};
+    while (file.read(data.data(), data.size()) || file.gcount() > 0) {
+        std::string_view chunk{data.data(), static_cast<std::size_t>(file.gcount())};
+        std::size_t count{};
+        for (unsigned char c : chunk) {
+            std::cout << "'\\x" << std::hex << std::setw(2) << std::setfill('0') << int{c} << "',";
+            count++;
+            if (count % 32 == 0) {
+                std::cout << '\n';
+            }
+        }
+    }
+    std::cout << "});\n";
+
+    std::cout << "constexpr auto " << name << " = std::string_view{" << name << "Bytes.data(), " << name
+              << "Bytes.size()};\n";
+    std::cout << "#endif // HST_GENERATED_" << name << "_H\n";
+}
