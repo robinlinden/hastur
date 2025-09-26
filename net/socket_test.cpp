5df7eebebf4ec196cc201f9c6e1bb6e2f50d5df8
Robin Linden <dev@robinlinden.eu>
2025-09-26 02:11:46 +0200

    net/test: Add a test for Socket::write

--- net/socket_test.cpp
+++ net/socket_test.cpp
@@ -7,22 +7,30 @@
 #include "etest/etest2.h"
 
 #include <asio/buffer.hpp>
+#include <asio/error.hpp>
+#include <asio/error_code.hpp>
 #include <asio/io_context.hpp>
 #include <asio/ip/address_v4.hpp>
 #include <asio/ip/tcp.hpp>
 #include <asio/write.hpp> // NOLINT: Needed for asio::write.
 
+#include <array>
 #include <cstdint>
+#include <cstdlib>
 #include <future>
+#include <iostream>
 #include <string>
 #include <thread>
 #include <utility>
+#include <variant>
 
 namespace {
 
 class Server {
 public:
     static Server writing_server(std::string response) { return Server{std::move(response)}; }
+    static Server receiving_server(std::promise<std::string> received_data) { return Server{std::move(received_data)}; }
+
     ~Server() { server_thread_.join(); }
 
     std::uint16_t port() { return port_future_.get(); }
@@ -31,19 +39,47 @@ private:
     std::thread server_thread_;
     std::future<std::uint16_t> port_future_;
 
-    explicit Server(std::string response) {
+    explicit Server(std::variant<std::string, std::promise<std::string>> input) {
         std::promise<std::uint16_t> port_promise;
         port_future_ = port_promise.get_future();
 
-        server_thread_ = std::thread{[payload = std::move(response), port = std::move(port_promise)]() mutable {
+        server_thread_ = std::thread{[payload = std::move(input), port = std::move(port_promise)]() mutable {
             asio::io_context io_context;
             constexpr int kAnyPort = 0;
             asio::ip::tcp::acceptor a{io_context, asio::ip::tcp::endpoint{asio::ip::address_v4::loopback(), kAnyPort}};
             port.set_value(a.local_endpoint().port());
 
             auto sock = a.accept();
-            // NOLINTNEXTLINE(misc-include-cleaner): Provided by <asio/write.hpp>.
-            asio::write(sock, asio::buffer(payload, payload.size()));
+
+            if (auto const *send_data = std::get_if<std::string>(&payload); send_data != nullptr) {
+                // NOLINTNEXTLINE(misc-include-cleaner): Provided by <asio/write.hpp>.
+                asio::write(sock, asio::buffer(*send_data, send_data->size()));
+                return;
+            }
+
+            std::string received;
+            std::array<char, 256> buf{};
+            asio::error_code ec;
+            while (true) {
+                auto n = sock.read_some(asio::buffer(buf), ec);
+                if (n > 0) {
+                    std::cerr << "Server received " << n << " bytes\n";
+                    received.append(buf.data(), n);
+                }
+
+                if (ec == asio::error::eof) {
+                    std::cerr << "Server reached EOF\n";
+                    break;
+                }
+
+                if (ec) {
+                    std::cerr << "Error reading from socket: " << ec.message() << '\n';
+                    std::abort();
+                }
+            }
+
+            auto &receive_promise = std::get<std::promise<std::string>>(payload);
+            receive_promise.set_value(std::move(received));
         }};
     }
 };
@@ -79,6 +115,21 @@ int main() {
         a.expect_eq(sock.read_bytes(3), "123");
         a.expect_eq(sock.read_bytes(2), "45");
         a.expect_eq(sock.read_bytes(4), "6789");
+    });
+
+    s.add_test("Socket::write", [](etest::IActions &a) {
+        std::promise<std::string> received_data;
+        auto future = received_data.get_future();
+
+        net::Socket sock;
+        auto server = Server::receiving_server(std::move(received_data));
+        a.require(sock.connect("localhost", std::to_string(server.port())));
+
+        a.expect_eq(sock.write("hello"), 5u);
+        a.expect_eq(sock.write(" world"), 6u);
+        a.expect(sock.disconnect());
+
+        a.expect_eq(future.get(), "hello world");
     });
 
     return s.run();
