6ce434ff03f7cabb4ac7df06f46bb1c215c37499
Robin Linden <dev@robinlinden.eu>
2026-02-15 20:38:14 +0100

    meta/clang-tidy: Enable the clang-analyzer and portability checks
    The clang-analyzer checks are enabled by default, but explicitly
    enabling them is clearer and ensures we always get all of them, even if
    the defaults change.

--- url/rtti_hack.h
+++ url/rtti_hack.h
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 Robin Lindén <dev@robinlinden.eu>
+// SPDX-FileCopyrightText: 2023-2026 Robin Lindén <dev@robinlinden.eu>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
@@ -12,6 +12,10 @@
 // icu needs to be compiled w/ rtti, and that means that any templates of theirs
 // that we instantiate also require rtti, so we instantiate them here to try to
 // shield the rest of the codebase from that.
-extern template class icu::StringByteSink<std::string>;
+//
+// portability-template-virtual-member-function: While this is gross, we test
+// this with all compilers we support, and the plan is for this to go away
+// together with icu soon.
+extern template class icu::StringByteSink<std::string>; // NOLINT(portability-template-virtual-member-function)
 
 #endif
