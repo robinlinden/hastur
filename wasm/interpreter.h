9d11d658c93cc8a4de21fa158bed84c37c5956cb
Alexander Dahlberg <dustbin4ever@gmail.com>
2026-08-26 23:11:21 +0200

    wasm: Complete interpreter support for i32 multiply

--- wasm/interpreter.h
+++ wasm/interpreter.h
@@ -269,6 +269,17 @@ public:
         return {};
     }
 
+    // https://webassembly.github.io/spec/core/exec/instructions.html#numeric-instructions
+    std::expected<void, Trap> interpret(instructions::I32Multiply const &) {
+        assert(stack.size() >= 2);
+        auto rhs = static_cast<std::uint32_t>(std::get<std::int32_t>(stack.back()));
+        stack.pop_back();
+        auto lhs = static_cast<std::uint32_t>(std::get<std::int32_t>(stack.back()));
+        stack.pop_back();
+        stack.emplace_back(static_cast<std::int32_t>(lhs * rhs));
+        return {};
+    }
+
     std::expected<void, Trap> interpret(instructions::I32Store const &v) {
         assert(stack.size() >= 2);
         auto [align, offset] = v.arg;
