91506c9bcc76925f9019bc64abd4f749ad90c5d2
Robin Linden <dev@robinlinden.eu>
2026-08-31 22:55:10 +0200

    css/test: Hide log spam when fuzzing the parser

--- css/parser_fuzz_test.cpp
+++ css/parser_fuzz_test.cpp
@@ -1,8 +1,11 @@
-// SPDX-FileCopyrightText: 2022 Robin Lindén <dev@robinlinden.eu>
+// SPDX-FileCopyrightText: 2022-2026 Robin Lindén <dev@robinlinden.eu>
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
 #include "css/parser.h"
+
+#include <spdlog/common.h>
+#include <spdlog/spdlog.h>
 
 #include <cstddef>
 #include <cstdint>
@@ -12,6 +15,8 @@
 extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *data, std::size_t size);
 
 extern "C" int LLVMFuzzerTestOneInput(std::uint8_t const *data, std::size_t size) {
+    // TODO(robinlinden): Nicer error handling and reporting.
+    spdlog::set_level(spdlog::level::off);
     std::ignore = css::parse(std::string_view{reinterpret_cast<char const *>(data), size});
     return 0;
 }
